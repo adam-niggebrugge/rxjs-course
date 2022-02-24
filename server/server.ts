1894bc54dfb9c557cc51e038c306f70c12244285
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2022-02-24 17:30:58 -0500

    Allow CORS for local

--- server/server.ts
+++ server/server.ts
@@ -9,6 +9,12 @@ const bodyParser = require('body-parser');
 
 const app: Application = express();
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(bodyParser.json());
 
 app.route('/api/courses').get(getAllCourses);
