89df085071f2ce4a9c4bfe1ba62b61b959904dec
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2022-02-24 18:00:02 -0500

    Move observable to utility folder for reuse

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -16,6 +16,7 @@ import {
 } from 'rxjs';
 import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
 import {createHttpObservable} from '../common/util';
+import { environment } from '../../environments/environment';
 
 
 @Component({
@@ -25,36 +26,17 @@ import {createHttpObservable} from '../common/util';
 })
 export class AboutComponent implements OnInit {
 
+    constructor() {};
+
     ngOnInit() {
-        //Placing a $ at the end of the variable means this is RxJs observable
-        const interval$ = timer(3000, 1000); //Define the stream time interval. 
+    
+     const http$ = createHttpObservable('/courses')
 
-        // //By subscribing to the definition, will result in a stream of number values that emit every 1 second
-        const sub = interval$.subscribe(val => {
-            console.log(`Stream 1 => ${val}`);
-        });
-
-        //This will allow us to unsubscribe from a stream after a set time. 
-        setTimeout(() => sub.unsubscribe(), 5000);
-        //Displays 
-        //Stream 1 => 0
-        //Stream 1 => 1
-        //then completes. only 2 values shown because timer waits 3 seconds to start, this time is counted for setTimeout() method
-
-        // interval$.subscribe(val => {
-        //     console.log(`Stream 22 => ${val}`);
-        // });
-        const click$ = fromEvent(document, 'click');
-
-        // Can have multiple arguments, next, error, completion
-        click$.subscribe(
-            
-            e => console.log(e),
-
-            err => console.log(err),
-
-            () => console.log(`Stream has completed`)
-        );
+      http$.subscribe(
+          courses => console.log(courses),
+          () => {}, //or noop no operation, simple
+          () => console.log('complete')
+      );
     }
 
   
--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,38 +1,24 @@
 import {Observable} from 'rxjs';
 
+import { environment } from '../../environments/environment';
 
 export function createHttpObservable(url:string) {
     return Observable.create(observer => {
+        
+        fetch(environment.apiUrl+url)
+        .then(response => {
+            return response.json();
+        })
+        .then(body => {
+            //We know we are getting back an object from the json() promise
+            observer.next(body);
+            //Want to end the stream after json body is returned
+            observer.complete();
+        })
+        .catch(err => {
+            //ensure that we respect the observable contract, if an error occurs like network issue
+            observer.error(err);
+        });
 
-        const controller = new AbortController();
-        const signal = controller.signal;
-
-        fetch(url, {signal})
-            .then(response => {
-
-                if (response.ok) {
-                    return response.json();
-                }
-                else {
-                    observer.error('Request failed with status code: ' + response.status);
-                }
-            })
-            .then(body => {
-
-                observer.next(body);
-
-                observer.complete();
-
-            })
-            .catch(err => {
-
-                observer.error(err);
-
-            });
-
-        return () => controller.abort()
-
-
-    });
+      });
 }
-
