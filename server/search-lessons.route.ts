db40e84c80fb9fc1c543d30a756cdff925df7cd2
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2022-02-24 12:36:32 -0500

    Updated to run locally without errors

--- server/search-lessons.route.ts
+++ server/search-lessons.route.ts
@@ -11,16 +11,16 @@ export function searchLessons(req: Request, res: Response) {
 
     const queryParams = req.query;
 
-    const courseId = queryParams.courseId,
-          filter = queryParams.filter || '',
+    const courseId = parseInt(queryParams.courseId as string),
+          filter:string = queryParams.filter as string || '',
           sortOrder = queryParams.sortOrder || 'asc',
-          pageNumber = parseInt(queryParams.pageNumber) || 0,
-          pageSize = parseInt(queryParams.pageSize) || 3;
+          pageNumber = parseInt(queryParams.pageNumber as string) || 0,
+          pageSize = parseInt(queryParams.pageSize as string) || 3;
 
     let lessons = Object.values(LESSONS).filter(lesson => lesson.courseId == courseId).sort((l1, l2) => l1.id - l2.id);
 
     if (filter) {
-       lessons = lessons.filter(lesson => lesson.description.trim().toLowerCase().search(filter.toLowerCase()) >= 0);
+       lessons = lessons.filter(lesson => lesson.description.trim().toLowerCase().search(filter.toLowerCase()) >= 0); //original
     }
 
     if (sortOrder == "desc") {
